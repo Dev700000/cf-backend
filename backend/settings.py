46f1ad1f1d10dfad3755ebab096f869e5d0cbddb
Ever zacapala <everlever300@gmail.com>
2025-08-11 15:12:00 -0600

    ALLOW HOSST

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ['https://cf-frontrd-yj62.vercel.app', 'cf-frontrd-yj62.vercel.app', 'localhost', '127.0.0.1']
 # [ "http://localhost:5173", "localhost", "127.0.0.1:5173" ]
 
 
